0a55daa74a495380cde0322f34cd4132c95aad47
Alec <hsu022210@gmail.com>
2016-01-22 13:23:16 +0800

    finshed spec above content and category

--- technews/parser.py
+++ technews/parser.py
@@ -6,42 +6,46 @@ def parser_page(url):
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'html.parser')
-    # print (soup.prettify().encode('utf-8'))
+
     url = url
+    source_press = soup.select("div.indent ul li a")[0]['href']
     title = soup.find('h1', {'class': 'entry-title'}).string
+
+    #____get time method 1
+    post_time_string = soup.select("header.entry-header table td span.body")[1].string
+
+    #____get time method 2
+    # for i in soup.findAll('span', {'class': 'head'}):
+    #     # import pdb; pdb.set_trace()
+    #     if i.string == '發布日期':
+    #         post_time_string = i.next_sibling.next_sibling.string
+    post_time = datetime.datetime.strptime(post_time_string, '%Y 年 %m 月 %d 日 %H:%M ')
+
     journalist = soup.find('a', {'rel': 'author'}).string
+
+    content =""
+    #____get content method 1
+    content_source_code = soup.select("div.indent p")
+    for i in content_source_code:
+        content += i.text
+
+    #____get content method 2
+    # for i in soup.findAll('div', {'class': 'indent'}):
+    #     for m in i.findAll('p'):
+    #         content += m.text
+
+
+    # ans = soup.select("div.indent p")
+    # print(ans[2])
+
     category = []
-    content = ""
-    for i in soup.findAll('li', {'class': 'menu-item-has-children'}):
-        # print(i.contents)
-        attr = i.contents[0]
-        item = attr.string
+    for i in soup.select('li.menu-item-has-children'):
+        category_title = i.contents[0]
+        item = category_title.string
         category.append(item)
-    # print(category)
-
-
-    for i in soup.findAll('div', {'class': 'indent'}):
-        for m in i.findAll('p'):
-            content += m.text
-
-# print(content)
-
-    # ans = soup.select("div.indent p")[0]
-    # print(ans)
-
-    # comment = soup.find('div', {'class': 'clearfix'})
-
-    for i in soup.findAll('span', {'class': 'head'}):
-        # import pdb; pdb.set_trace()
-        if i.string == '發布日期':
-
-            print(i)
-            target = i.next_sibling.string
-            print(target)
-
-    # fb_like = soup.find('span', {'class': 'pluginCountTextDisconnected'}).string
 
 parser_page('http://technews.tw/2016/01/04/tiobe-2015-programming-language-index/')
+# parser_page('http://technews.tw/2016/01/20/elon-musk-isnt-worried-about-an-apple-electric-car-it-will-expand-the-industry/')
 
 
 def get_category_urls(category_url):
