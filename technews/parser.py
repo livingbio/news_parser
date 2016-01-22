12fdce3bcb79de1d9d487f77c27242345aefd8a9
Alec <hsu022210@gmail.com>
2016-01-22 18:54:01 +0800

    finished all except subcomments

--- technews/parser.py
+++ technews/parser.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 import json
+from pytz import timezone, all_timezones
 
 def parser_page(url):
     source_code = requests.get(url)
@@ -22,7 +23,6 @@ def parser_page(url):
 
     #____get time method 2
     # for i in soup.findAll('span', {'class': 'head'}):
-    #     # import pdb; pdb.set_trace()
     #     if i.string == '發布日期':
     #         post_time_string = i.next_sibling.next_sibling.string
     post_time = datetime.datetime.strptime(post_time_string, '%Y 年 %m 月 %d 日 %H:%M ')
@@ -40,15 +40,12 @@ def parser_page(url):
     #     for m in i.findAll('p'):
     #         content += m.text
 
-
-    # ans = soup.select("div.indent p")
-    # print(ans[2])
-
     category = []
     for i in soup.select('li.menu-item-has-children'):
         category_title = i.contents[0]
         item = category_title.string
         category.append(item)
+
 
     def fb_plugin_count_page(url):
         code = requests.get('http://api.facebook.com/restserver.php?method=links.getStats&urls=' + url)
@@ -57,22 +54,43 @@ def parser_page(url):
 
         fb_like_count = fb_plugin_page_soup.find('total_count').string
         fb_share_count = fb_plugin_page_soup.find('share_count').string
-
         return(fb_like_count, fb_share_count)
 
+
     fb_like, fb_share = fb_plugin_count_page(url)
+
 
     def fb_plugin_comment_page(url):
         code = requests.get('http://graph.facebook.com/comments?id=' + url)
         html_text = code.text
         fb_plugin_page_soup = BeautifulSoup(html_text, 'html.parser')
-        return (str(fb_plugin_page_soup))
-    fb_comments = fb_plugin_comment_page(url)
-    ans = json.loads(fb_comments)
-    print(ans)
+        fb_comments_string = str(fb_plugin_page_soup)
+        fb_comments_json_page = json.loads(fb_comments_string)
+        return (fb_comments_json_page)
 
-parser_page('http://technews.tw/2016/01/04/tiobe-2015-programming-language-index/')
-# parser_page('http://technews.tw/2016/01/06/iphone-6s-no-good-apple/')
+
+    fb_comments_json = fb_plugin_comment_page(url)
+
+
+    total_comments = []
+    for comment in fb_comments_json['data']:
+        comment_time_in_US = datetime.datetime.strptime(comment['created_time'], '%Y-%m-%dT%H:%M:%S%z')
+        comment_time_in_TW = comment_time_in_US.astimezone(timezone('ROC'))
+
+        each_comment = {
+            'actor': comment['from']['name'],
+            'like': comment['like_count'],
+            'content': comment['message'],
+            'post_time': comment_time_in_TW,
+            'source_type': 'facebook',
+            'sub_comments':[],
+        }
+        total_comments.append(each_comment)
+
+    print(total_comments)
+
+# parser_page('http://technews.tw/2016/01/04/tiobe-2015-programming-language-index/')
+parser_page('http://technews.tw/2016/01/06/iphone-6s-no-good-apple/')
 
 
 def get_category_urls(category_url):
@@ -85,22 +103,3 @@ def get_category_urls(category_url):
     return(detail_url)
 
 # get_category_urls('http://technews.tw/category/tablet/')
-
-
-# def crawling(max_pages):
-#     page = 20106
-#     while page <= max_pages:
-#         url = 'https://www.thenewboston.com/videos.php?cat=98&video=' + str(page)
-#         source_code = requests.get(url)
-#         plain_text = source_code.text
-#         soup = BeautifulSoup(plain_text, 'html.parser')
-#         # print (soup.prettify())
-#         for i in soup.findAll('span', {'class': 'text-bold' } ):
-#             title = i.string
-#             print(title)
-#
-#         page += 1
-#
-#     print('---done---')
-#
-# crawling(20106+5)
