042dc5589e45aa48b3f2dac3bd89c491964560f3
Alec <hsu022210@gmail.com>
2016-01-23 20:07:12 +0800

    finished all the spec and thinking of i can write a function to swtich page as well

--- technews/parser.py
+++ technews/parser.py
@@ -4,46 +4,34 @@ import datetime
 import json
 from pytz import timezone, all_timezones
 
+
 def parser_page(url):
     source_code = requests.get(url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'html.parser')
 
+
     url = url
     source_press = soup.select("div.indent a")[1]['href']
-    
-    title = soup.find('h1', {'class': 'entry-title'}).string
-
+    title = soup.find('h1', {'class': 'entry-title'}).text
 
     #____get time method 1
-    post_time_string = soup.select("header.entry-header table td span.body")[1].string
+    post_time_text = soup.select("header.entry-header table td span.body")[1].text
 
     #____get time method 2
     # for i in soup.findAll('span', {'class': 'head'}):
-    #     if i.string == '發布日期':
-    #         post_time_string = i.next_sibling.next_sibling.string
-    post_time = datetime.datetime.strptime(post_time_string, '%Y 年 %m 月 %d 日 %H:%M ')
+    #     if i.text == '發布日期':
+    #         post_time_text = i.next_sibling.next_sibling.text
+    post_time = datetime.datetime.strptime(post_time_text, '%Y 年 %m 月 %d 日 %H:%M ')
 
-    journalist = soup.find('a', {'rel': 'author'}).string
+
+    journalist = soup.find('a', {'rel': 'author'}).text
+
 
     content =""
-    #____get content method 1
     content_source_code = soup.select("div.indent p")
-    for i in content_source_code:
-        content += i.text
-
-    # print(content)
-
-    #____get content method 2
-    # for i in soup.findAll('div', {'class': 'indent'}):
-    #     for m in i.findAll('p'):
-    #         content += m.text
-
-    category = []
-    for i in soup.select('li.menu-item-has-children'):
-        category_title = i.contents[0]
-        item = category_title.string
-        category.append(item)
+    for i in range(len(content_source_code)-1):
+        content += content_source_code[i].text
 
 
     def fb_plugin_count_page(url):
@@ -54,9 +42,14 @@ def parser_page(url):
         fb_like_count = fb_plugin_page_soup.find('total_count').string
         fb_share_count = fb_plugin_page_soup.find('share_count').string
         return(fb_like_count, fb_share_count)
-
-
     fb_like, fb_share = fb_plugin_count_page(url)
+
+
+    category = []
+    for i in soup.select('li.menu-item-has-children'):
+        category_title = i.contents[0]
+        item = category_title.text
+        category.append(item)
 
 
     def fb_plugin_comment_page(url):
@@ -67,58 +60,80 @@ def parser_page(url):
         fb_comments_string = str(fb_plugin_page_soup)
         fb_comments_json_page = json.loads(fb_comments_string)
         return (fb_comments_json_page)
-
-
     fb_comments_json = fb_plugin_comment_page(url)
-
 
     total_comments = []
 
-    for comment in fb_comments_json['data']:
-        comment_time_in_US = datetime.datetime.strptime(comment['created_time'], '%Y-%m-%dT%H:%M:%S%z')
-        comment_time_in_TW = comment_time_in_US.astimezone(timezone('ROC'))
+    def make_fb_comments_dictionary(fb_comments_json):
+        for comment in fb_comments_json['data']:
+            comment_time_in_US = datetime.datetime.strptime(comment['created_time'], '%Y-%m-%dT%H:%M:%S%z')
+            comment_time_in_TW = comment_time_in_US.astimezone(timezone('ROC'))
 
-        while True:
-            try:
-                if comment['parent']['id']:
-                    for each_comment in total_comments:
-                        if each_comment['id'] == comment['parent']['id']:
-                            each_sub_comment = {
-                                'id': comment['id'],
-                                'actor': comment['from']['name'],
-                                'like': comment['like_count'],
-                                'content': comment['message'],
-                                'post_time': comment_time_in_TW,
-                                'source_type': 'facebook',
-                            }
-                            each_comment['sub_comments'].append(each_sub_comment)
-            except KeyError:
-                each_comment = {
-                    'id': comment['id'],
-                    'actor': comment['from']['name'],
-                    'like': comment['like_count'],
-                    'content': comment['message'],
-                    'post_time': comment_time_in_TW,
-                    'source_type': 'facebook',
-                    'sub_comments': [],
-                }
-                total_comments.append(each_comment)
-            finally:
-                break
+            while True:
+                try:
+                    if comment['parent']['id']:
+                        for each_comment in total_comments:
+                            if each_comment['id'] == comment['parent']['id']:
+                                each_sub_comment = {
+                                    'id': comment['id'],
+                                    'actor': comment['from']['name'],
+                                    'like': comment['like_count'],
+                                    'content': comment['message'],
+                                    'post_time': comment_time_in_TW,
+                                    'source_type': 'facebook',
+                                }
+                                each_comment['sub_comments'].append(each_sub_comment)
+                except KeyError:
+                    each_comment = {
+                        'id': comment['id'],
+                        'actor': comment['from']['name'],
+                        'like': comment['like_count'],
+                        'content': comment['message'],
+                        'post_time': comment_time_in_TW,
+                        'source_type': 'facebook',
+                        'sub_comments': [],
+                    }
+                    total_comments.append(each_comment)
+                finally:
+                    break
+    make_fb_comments_dictionary(fb_comments_json)
 
 
-parser_page('http://technews.tw/2016/01/04/tiobe-2015-programming-language-index/')
-# parser_page('http://technews.tw/2016/01/06/iphone-6s-no-good-apple/')
-# parser_page('http://technews.tw/2015/11/26/apple-iphone-2018-oled-﻿panel/')
+    result = {
+        "url": url,
+        "source_press": source_press,
+        "title": title,
+        "post_time": post_time,
+        "journalist": journalist,
+        "content": content,
+        "compare": None,
+        "keyword": [],
+        "fb_like": fb_like,
+        "fb_share": fb_share,
+        "category": category,
+        "comment": total_comments,
+    }
 
+
+    return(result)
+
+# page_data = parser_page('http://technews.tw/2016/01/04/tiobe-2015-programming-language-index/')
+# page_data = parser_page('http://technews.tw/2016/01/06/iphone-6s-no-good-apple/')
+page_data = parser_page('http://technews.tw/2015/11/26/apple-iphone-2018-oled-﻿panel/')
+# print(page_data)
 
 def get_category_urls(category_url):
     source_code = requests.get(category_url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, 'html.parser')
     detail_url = []
+
     for i in soup.findAll('h1', {'class': 'entry-title'}):
         detail_url.append(i.a.attrs['href'])
-    return(detail_url)
+    return (detail_url)
 
-# get_category_urls('http://technews.tw/category/tablet/')
+crawling_urls_list = get_category_urls('http://technews.tw/category/tablet/')
+
+# for i in crawling_urls_list:
+#     ans = parser_page(i)
+#     print(ans)
